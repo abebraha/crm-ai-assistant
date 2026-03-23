a5f89642ff977117c701b4215c65f26d5d80d229
abebraha <abe@icebreakerbd.com>
2026-03-24 01:29:09 +0200

    feat: add lib/db.ts

---
+++ lib/db.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from '@prisma/client';
+
+// Prevent multiple instances of Prisma Client in development (hot reloading)
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ log: ['error'] });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
