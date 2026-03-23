ef3c924937f22b8bae69f427cafda5eee5b43688
abebraha <abe@icebreakerbd.com>
2026-03-24 01:25:30 +0200

    feat: add app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import ChatInterface from '@/components/ChatInterface';
+
+export default function Home() {
+  return (
+    <main className="flex flex-col h-screen bg-slate-50">
+      <ChatInterface />
+    </main>
+  );
+}
